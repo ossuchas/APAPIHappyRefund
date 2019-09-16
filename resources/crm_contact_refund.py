495306a61951206acfae1311cfb76afec83ea856
ossuchas <suchat.s14102526@gmail.com>
2019-09-17 05:31:02 +0700

    modified personal id validate

--- resources/crm_contact_refund.py
+++ resources/crm_contact_refund.py
@@ -57,15 +57,21 @@ class CrmContactRefundPersonalIdList(Resource):
     def get(cls, personal_id: str):
         hyrf = CrmContactRefundModel.find_by_personalid(personal_id)
         if hyrf:
-            _contractnumber = hyrf.contractnumber
-            _hyrf = CrmContactRefundModel.validate_id_sent(_contractnumber)
-
-            if not _hyrf:
-                return hyrf_list_schema.dump(CrmContactRefundModel.find_by_contract(_contractnumber)), 200
-            else:
-                return {"message": "Transaction already submit document"}, 404
+            return hyrf_list_schema.dump(CrmContactRefundModel.find_by_personalidlist(personal_id)), 200
 
         return {"message": "No Data Found"}, 404
+    # def get(cls, personal_id: str):
+    #     hyrf = CrmContactRefundModel.find_by_personalid(personal_id)
+    #     if hyrf:
+    #         _contractnumber = hyrf.contractnumber
+    #         _hyrf = CrmContactRefundModel.validate_id_sent(_contractnumber)
+    #
+    #         if not _hyrf:
+    #             return hyrf_list_schema.dump(CrmContactRefundModel.find_by_contract(_contractnumber)), 200
+    #         else:
+    #             return {"message": "Transaction already submit document"}, 404
+    #
+    #     return {"message": "No Data Found"}, 404
 
 
 class CrmCustomerRefund(Resource):
