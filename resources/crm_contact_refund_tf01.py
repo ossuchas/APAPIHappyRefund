9c377932fdab4948daa71bab4e9fc63653d89c5e
ossuchas <suchat.s14102526@gmail.com>
2019-10-07 12:35:50 +0700

    modified replace string - to space

--- resources/crm_contact_refund_tf01.py
+++ resources/crm_contact_refund_tf01.py
@@ -39,7 +39,7 @@ class CrmContactRefundTF01(Resource):
             hyrf.tf01_appv_by = item_json["tf01_appv_by"]
             hyrf.tf01_remarks = item_json["tf01_remarks"]
             hyrf.bankcode = item_json["bankcode"]
-            hyrf.bankaccountno = item_json["bankaccountno"]
+            hyrf.bankaccountno = item_json["bankaccountno"].replace('-', '')
         else:
             return {"message": "Can not find Refund ID for update"}, 404
 
