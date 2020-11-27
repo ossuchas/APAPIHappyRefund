385439c2008afa29a1edad4850ce7e31386b3041
เรารักเมียหลวง <>
2020-11-27 14:34:18 +0700

    modified tf02 search

--- models/crm_contact_refund.py
+++ models/crm_contact_refund.py
@@ -186,13 +186,22 @@ class CrmContactRefundModel(db.Model):
     @classmethod
     def find_txt_search_tf02_appv(cls, str_search: str) -> List["CrmContactRefundModel"]:
         return cls.query.filter(
-            (cls.tf02_appv_flag == 'A'), (cls.tran_status == 'A'),
-            ((cls.unitnumber.like('%' + str_search + '%')) |
-            (cls.productid.like('%' + str_search + '%')) |
-            (cls.project.like('%' + str_search + '%')) |
-            (cls.fullname.like('%' + str_search + '%')) |
-            (cls.coownername.like('%' + str_search + '%')))
+            (cls.tf02_appv_flag == 'N'), (cls.tf01_appv_flag == 'A'), (cls.tran_status == 'A'),
+            (
+                    (cls.unitnumber.like('%' + str_search + '%')) | (cls.productid.like('%' + str_search + '%')) |
+                    (cls.project.like('%' + str_search + '%')) | (cls.fullname.like('%' + str_search + '%')) |
+                    (cls.coownername.like('%' + str_search + '%'))
+            )
         ).order_by(cls.modifydate.desc()).all()
+
+        # return cls.query.filter(
+        #     (cls.tf02_appv_flag == 'A'), (cls.tran_status == 'A'),
+        #     ((cls.unitnumber.like('%' + str_search + '%')) |
+        #     (cls.productid.like('%' + str_search + '%')) |
+        #     (cls.project.like('%' + str_search + '%')) |
+        #     (cls.fullname.like('%' + str_search + '%')) |
+        #     (cls.coownername.like('%' + str_search + '%')))
+        # ).order_by(cls.modifydate.desc()).all()
 
     @classmethod
     def find_all_tf02_rejt(cls) -> List["CrmContactRefundModel"]:
